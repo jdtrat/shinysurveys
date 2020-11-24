12f1b54e12309a7b45455b74318626cd91f71358
Jonathan Trattner <accounts@jdtrat.com>
2020-11-24 13:10:33 -0500

    Updated UI

--- R/func_question_ui.R
+++ R/func_question_ui.R
@@ -8,49 +8,55 @@
 #' @export
 
 flex_form_question_ui <- function(question_number) {
-  shiny::tagList(shiny::fluidRow(
-    shiny::column(
-      width = 4,
-      offset = 0,
-      shiny::textInput(paste0("question_", question_number, "_title"),
-                       "",
-                       "Untitled Question",
-                       width = "600px"),
-    ),
-    shiny::column(width = 2,
-                  offset = 0.5,
-                  shiny::selectInput(paste0("question_", question_number, "_type"),
-                                     "",
-                                     choices = c(
-                                       "Select",
-                                       "Numeric",
-                                       "Multiple Choice",
-                                       "Text",
-                                       "Yes/No"
+  shiny::tagList(div(class = "survey",
+                     shiny::fluidRow(
+                       shiny::column(
+                         width = 4,
+                         offset = 0,
+                         shiny::textInput(paste0("question_", question_number, "_title"),
+                                          "",
+                                          "Untitled Question",
+                                          width = "600px"),
+                       ),
+                       shiny::column(width = 2,
+                                     offset = 0.5,
+                                     shiny::selectInput(paste0("question_", question_number, "_type"),
+                                                        "",
+                                                        choices = c(
+                                                          "Select",
+                                                          "Numeric",
+                                                          "Multiple Choice",
+                                                          "Text",
+                                                          "Yes/No"
+                                                        )
                                      )
-                  )
-    ),
-    shiny::div(id = paste0("option_placeholder_", question_number)),
-  ),
-  shiny::fluidRow(
-    shiny::column(
-      width = 1,
-      offset = 2,
-      shiny::actionButton(
-        inputId = paste0("question_", question_number, "_title_", "remove"),
-        label = "Remove",
-        icon = shiny::icon("trash")
-      )
-    ),
-    shiny::column(width = 1,
-                  offset = 2.9,
-                  shinyWidgets::switchInput(
-                    inputId = paste0("question_", question_number, "_title_", "required"),
-                    label = "Required",
-                    labelWidth = "60px"
-                  )
-    )
-  ),
-  shiny::div(id = paste0("question_", c(question_number + 1)))
+                       ),
+                       shiny::div(id = "option_placeholder_1",
+                                  shiny::textInput(inputId = "question_1_option_1",
+                                                   label = "Option 1",
+                                                   value = "Placeholder")),
+                       shiny::div(id = paste0("option_placeholder_", question_number)),
+                     ),
+                     shiny::fluidRow(
+                       shiny::column(
+                         width = 1,
+                         offset = 2,
+                         shiny::actionButton(
+                           inputId = paste0("question_", question_number, "_title_", "remove"),
+                           label = "Remove",
+                           icon = shiny::icon("trash")
+                         )
+                       ),
+                       shiny::column(width = 1,
+                                     offset = 2.9,
+                                     shinyWidgets::switchInput(
+                                       inputId = paste0("question_", question_number, "_title_", "required"),
+                                       label = "Required",
+                                       labelWidth = "60px"
+                                     )
+                       )
+                     ),
+                     shiny::div(id = paste0("question_", c(question_number + 1)))
+  )
   )
 }
