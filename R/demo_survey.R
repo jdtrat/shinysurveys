8708726b6973536cd1a6a47939a8269dc2f13679
Jonathan Trattner <accounts@jdtrat.com>
2020-11-25 18:05:52 -0500

    Updated demo_survey

--- R/demo_survey.R
+++ R/demo_survey.R
@@ -15,49 +15,134 @@
 #' @examples
 #'
 #' \dontrun{
-#' demo_survey_page(teaching_r_questions)
+#' demo_survey()
 #' }
-demo_survey <- function(questions) {
+demo_survey <- function() {
 
-  # I created a local directory
-  outputDir <- "~/Downloads/"
-
-  # function based on Dean Attali's post about persistent data storage
-  # https://shiny.rstudio.com/articles/persistent-data-storage.html
-  saveData <- function(data, userID) {
-
-    # Create a unique file name
-    fileName <- base::sprintf("%s_%s.csv", userID, base::as.integer(base::Sys.time()))
-
-    # Write the file to the local system
-    utils::write.csv(data,
-                     file = base::file.path(outputDir, fileName))
-  }
-
+  require("shiny")
+  require("shinyjs")
+  require("shinyWidgets")
 
   ui <- shiny::fluidPage(
-    surveyOutput(questions)
+    shiny::tags$head(
+      shiny::tags$style(
+        shiny::HTML('
+            @import url("https://fonts.googleapis.com/css?family=Source+Code+Pro|Montserrat|Raleway");
+@import url("https://fonts.googleapis.com/css?family=Inconsolata");
+.grid {
+  display: grid;
+  overflow: scroll;
+  width: 100%;
+  height: 100%;
+  grid-template-columns: 1fr 1fr 1fr;
+  background-color: #e8f4f8;
+}
+
+.container-fluid {
+  background-color: #c1e1ec;
+}
+
+.survey {
+  padding: 20px;
+  /* border: 1px solid black; */
+  grid-column-start: 2;
+  grid-column-end: 2;
+  /* justify-items: center;
+  justify-self: center; */
+  position: center;
+  background-color: #c1e1ec;
+}
+
+.input-pane {
+  grid-column-start: 1;
+  padding: 20px;
+  border: 1px solid black;
+  grid-column-end: 1;
+  position: fixed;
+  background-color: #d4ebf2;
+}
+
+body {
+  font-family: \'Raleway\', sans-serif;
+  background-color: #e8f4f8;
+}
+
+code {
+  font-family: \'Source Code Pro\', monospace;
+  font-size: 14px;
+}
+
+.console {
+  font-family: \'Inconsolata\', monospace;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: black;
+  opacity: .8;
+  padding: 20px;
+  color: white;
+  z-index: 99999;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  color: #333;
+  font-family: \'Montserrat\', sans-serif;
+}
+
+h1, h2, h3 {
+  text-transform: uppercase;
+  text-align: left;
+  letter-spacing: .1em;
+  line-height: 1.2;
+}
+
+h1 {
+  margin: 36px 0;
+}
+
+h1.title {
+  color: #416983;
+}
+
+h3 {
+  font-style: italic;
+  font-family: \'Montserrat\', sans-serif;
+}
+
+p {
+  color: #333;
+  margin: 35px;
+  margin-bottom: 10px;
+}
+
+li.l {
+  margin-left: 40px;
+  margin-right: 35px;
+}
+
+')
+      )
+    ),
+div(class = "grid",
+    div(class = "survey",
+        h1("Sample Survey"),
+        surveyOutput(teaching_r_questions)
+        )
+    )
   )
 
   server <- function(input, output, session) {
 
-    renderSurvey(input = input, df = questions, session = session)
+    renderSurvey(input = input, df = teaching_r_questions, session = session)
 
-    formData <- shiny::reactive({
-      data <- tibble::tribble(~userID, ~question, ~response,
-                              input$userID, "age", as.character(input$age),
-                              input$userID, "gender", input$gender,
-                              input$userID, "education_attained", input$education_attained,
-                              input$userID, "first_language", input$first_language,
-                              input$userID, "read_language", input$read_language,
-                              input$userID, "learned_r", input$learned_r,
-                              input$userID, "learned_programming_not_r",input$learned_programming_not_r,
-                              input$userID, "completed_data_analysis", input$completed_data_analysis
-      )
-      data
+    shiny::observeEvent(input$submit, {
+
+      showModal(modalDialog(
+        title = "Congrats, you submitted your first shinysurvey!",
+        "You can customize what actions happen when a user finishes a survey using input$submit."
+      ))
+
     })
-
-    shiny::observeEvent(input$submit, {saveData(data = formData(), userID = input$userID)})
 
   }
 
