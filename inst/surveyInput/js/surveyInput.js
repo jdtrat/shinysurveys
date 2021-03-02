0ab6fe7597048fc86a643b5741f8033da1951441
Jonathan Trattner <accounts@jdtrat.com>
2021-03-02 17:43:14 -0500

    Work in progress on surveyInput binding. Need to generalize the return for all options and ensure when the add option button is clicked, only ONE new option is added.

--- inst/surveyInput/js/surveyInput.js
+++ inst/surveyInput/js/surveyInput.js
@@ -1,3 +1,6 @@
+// initialize option_number as 1 since we start with one option
+var option_number = 1;
+
 var shinySurveyBinding = new Shiny.InputBinding();
 $.extend(shinySurveyBinding, {
 
@@ -10,23 +13,27 @@ $.extend(shinySurveyBinding, {
   // Get the value of the custom input
   getValue: function getValue(el) {
 
-    title = $(el).find('#question_title').val();
-
+    let question = $(el).find('#question_title').val();
+    let input_type = $(el).find('#question_type').val();
     // remove punctuation and replace spaces with underscores and make all lowercase for input_id
-    identifier = title.replace(/[.,?'"\/#!$%\^&\*;:{}=\-_`~()]/g, '').split(' ').join('_').toLowerCase();
+    let input_id = question.replace(/[^a-zA-Z0-9_-]/g, '').split(' ').join('_').toLowerCase();
+    let dependence = 'NA';
+    let dependence_value = 'NA';
+    let required = $(el).find('#question_required').prop('checked');
 
     // return values (data frame in R) of the form used in {shinysurveys}
     var values = [{
-      "question": title,
-      "option": $(el).find('#option_1').val(),
-      "input_type": $(el).find('#question_type').val(),
-      "input_id": identifier,
-      "dependence": 'NA', // GUI doesn't deal with dependencies now
-      "dependence_value": 'NA', // GUI doesn't deal with dependencies now
-      "question_required": $(el).find('#question_required').prop('checked')
+      "question": question,
+      "option": $(el).find('#option_1').val(), // work on generalizing for any option
+      "input_type": input_type,
+      "input_id": input_id,
+      "dependence": dependence, // GUI doesn't deal with dependencies now
+      "dependence_value": dependence_value, // GUI doesn't deal with dependencies now
+      "required": required
     }];
 
     return values
+
   },
 
   subscribe: function(el, callback) {
@@ -36,6 +43,27 @@ $.extend(shinySurveyBinding, {
     $(el).find("#question_required").change(function(evt) {callback();})
     $(el).find("#option_1").on("keyup", function(evt) {callback();})
 
+
+    // When the add_option button is clicked, insert another input element
+    $(el).find("#add_option").on("click", function() {
+
+      // unbind all Shiny inputs
+      Shiny.unbindAll();
+
+      // increment the option_number
+      option_number++;
+
+      // append to the option_placeholder a new input element
+      $(el).find('#option_placeholder').append(
+        '<div class=\"form-group shiny-input-container\" style=\"width:69%;\">' +
+        '<label class=\"control-label\" id=\"option_' + option_number + 'label\" for=\"option_' + option_number + '\"></label>' +
+        '<input id=\"option_' + option_number + '\" type=\"text\" class=\"form-control\" value=\"Placeholder\"/>' +
+        '</div>'
+      );
+
+      // Bind all shiny inputs, including the newly inserted one.
+      Shiny.bindAll();
+    })
   },
   unsubscribe: function(el) {
     $(el).off(".shinySurveyBinding");
