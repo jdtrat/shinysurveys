c35ad25189ce443ce3d533da075d957575c3ad3e
Jonathan Trattner <accounts@jdtrat.com>
2021-07-06 11:30:52 -0400

    Added dependency flag for getSurveyData to (by default) ensure a consistent output with one row per unique input ID (question).

--- R/save_data.R
+++ R/save_data.R
@@ -52,8 +52,28 @@ check_length <- function(.input) {
 #'
 #' @param custom_id A unique identifier for the survey's respondents. NULL by
 #'   default, and the built-in {shinysurveys} userID will be used.
+#' @param include_dependencies LOGICAL: TRUE (default) and all dependency
+#'   questions will be returned, regardless of if the individual respondent saw
+#'   it. For respondents who did not see a specific question, the 'response'
+#'   will take on the value from the `dependency_string` argument. If FALSE, the
+#'   output will have variable rows depending on which questions a given
+#'   participant answered.
+#' @param dependency_string A character string to be imputed for dependency
+#'   questions that a respondent did not see. Default is "HIDDEN-QUESTION".
 #'
-#' @return A participant's responses.
+#' @return A data frame with four columns containing information about the
+#'   participant's survey responses: The 'subject_id' column can be used for
+#'   identifying respondents. By default, it utilizes shinysurveys URL-based
+#'   user tracking feature. The 'question_id' and 'question_type' columns
+#'   correspond to  'input_id' and 'input_type' from the original data frame of
+#'   questions. The 'response' column is the participant's answer.
+#'
+#'   The number of rows, corresponding to the questions an individual saw,
+#'   depends on the `include_dependencies` argument. If TRUE, by default, then
+#'   the resulting data frame will have one row per unique input ID. If FALSE,
+#'   the data frame may have variable length depending on which questions a
+#'   given individual answers.
+#'
 #' @export
 #'
 #' @examples
@@ -78,13 +98,13 @@ check_length <- function(.input) {
 #'
 #'  }
 #'
-getSurveyData <- function(custom_id = NULL) {
+getSurveyData <- function(custom_id = NULL, include_dependencies = TRUE, dependency_string = "HIDDEN-QUESTION") {
 
   session <- shiny::getDefaultReactiveDomain()
 
   # get id of instructions input types to exclude from survey response collection
   instructions_id <- survey_env$question_df[which(survey_env$question_df$input_type == "instructions"), "input_id", drop = FALSE]$input_id
-  shown_questions <- unique(survey_env$question_df$input_id[which(!survey_env$question_df$input_id %in% c(session$input$shinysurveysHiddenInputs, instructions_id))])
+  shown_questions <- unique(survey_env$question_df$input_id[which(!survey_env$question_df$input_id %in% instructions_id)])
 
   for (i in seq_along(survey_env$unique_questions)) {
     survey_env$unique_questions[[i]]$question_number <- rep(i, nrow(survey_env$unique_questions[[i]]))
@@ -143,12 +163,17 @@ getSurveyData <- function(custom_id = NULL) {
                     output)
   }
 
-
   output <- split(output, factor(output$question_id, levels = unique(output$question_id)))
   output <- do.call(rbind, lapply(
     output, function(x) x[1,]
   ))
   rownames(output) <- NULL
+
+  if (include_dependencies) {
+    output[which(output$question_id %in% session$input$shinysurveysHiddenInputs), "response"] <- dependency_string
+  } else if (!include_dependencies) {
+    output <- output[which(!output$question_id %in% session$input$shinysurveysHiddenInputs),]
+  }
 
   return(output)
 
